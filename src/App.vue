37062818bb479753faa88e6af5b147ef7f60e8b9
Natalina-Yarosh <natali.yarosh.96@mail.ru>
2025-03-17 00:10:28 +0300

    Установка проекта

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import AppHeader from '@/components/AppHeader.vue'
+</script>
+
+<template>
+  <div class="container">
+    <app-header />
+    <div class="content">
+      <RouterView />
+    </div>
+  </div>
+</template>
+
+
+<style scoped>
+  .container{
+    max-width: 1280px;
+    padding: 20px;
+    margin: auto;
+  }
+</style>
---
+++ src/main.ts
@@ -0,0 +1,31 @@
+import './assets/main.css'
+import 'primeicons/primeicons.css'
+import 'primeflex/primeflex.css';
+
+import Aura from '@primeuix/themes/aura';
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config';
+import Menubar from 'primevue/menubar';
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura,
+        options: {
+            darkModeSelector: false || 'none',
+        }
+    }
+});
+app.use(router)
+app.use(PrimeVue);
+app.component('app-menubar', Menubar);
+
+
+app.mount('#app')
