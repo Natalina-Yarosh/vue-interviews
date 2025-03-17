729c79ab69ff136f53b0722b7991ddcacf8fdb82
Natalina-Yarosh <natali.yarosh.96@mail.ru>
2025-03-18 00:06:44 +0300

    Отображение и удаление данных

--- src/main.ts
+++ src/main.ts
@@ -10,13 +10,20 @@ import { initializeApp } from "firebase/app";
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config';
+import ToastService from 'primevue/toastservice';
+import ConfirmationService from 'primevue/confirmationservice';
+
 import Menubar from 'primevue/menubar';
 import Button from 'primevue/button';
 import InputText from 'primevue/inputtext';
 import Toast from 'primevue/toast';
-import ToastService from 'primevue/toastservice';
+
 import ProgressSpinner from 'primevue/progressspinner';
 import Card from 'primevue/card';
+import  DataTable  from 'primevue/datatable';
+import  Column  from 'primevue/column';
+import  ConfirmDialog  from 'primevue/confirmdialog';
+import  InlineMessage  from 'primevue/inlinemessage';
 
 import App from './App.vue'
 import router from './router'
@@ -37,6 +44,7 @@ initializeApp(firebaseConfig);
 const app = createApp(App)
 
 app.use(ToastService)
+app.use(ConfirmationService)
 app.use(createPinia())
 app.use(PrimeVue, {
     theme: {
@@ -53,5 +61,9 @@ app.component('app-input-text', InputText);
 app.component('app-toast', Toast);
 app.component('app-progress', ProgressSpinner);
 app.component('app-card', Card);
+app.component('app-datatable', DataTable);
+app.component('app-column', Column);
+app.component('app-dialog', ConfirmDialog);
+app.component('app-message', InlineMessage);
 
 app.mount('#app')
